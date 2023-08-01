c9db116428dc022babaf72f94fe15c019a11a47f
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-07-31 20:17:35 -0400

    changed the convex in main.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 // import.meta.env.VITE_CONVEX_URL as string
-const convex = new ConvexReactClient("https://mild-snake-257.convex.cloud");
+const convex = new ConvexReactClient("https://hardy-coyote-816.convex.cloud");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
